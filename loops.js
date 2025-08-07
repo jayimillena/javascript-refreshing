757ba927fbaa9846e585d3adb391873a5b332148
Jay Millena <jay.millena07@gmail.com>
2025-08-07 14:27:57 +0800

    Looping using array

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
 for (let i=0; i < 5; i++) {
     console.log("Count:" + i); // Outputs numbers from 0 to 4
 }
+
+let fruits = ["apple", "banana", "cherry"];
+for (let fruit of fruits) { 
+    console.log(fruit); // Outputs each fruit in the array
+}
