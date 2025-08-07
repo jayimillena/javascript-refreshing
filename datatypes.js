cb3f48ecfa4e451a650d9e1159b39c264a0a0736
Jay Millena <jay.millena07@gmail.com>
2025-08-07 14:13:25 +0800

    JavaScript on datatypes

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+/*
+
+String -> "Hello"
+Number -> 123, 3.14
+Boolean -> true or false
+Array -> ["Apple", "Banana", "Cherry"]
+Object -> { name: "Jay", age: 35 }
+Null -> empty
+undefined -> not assigned
+
+*/
--- variables.js
+++ variables.js
@@ -1,3 +1,3 @@
 let name = "Jay";   // can be updated
-const age = 25;     // cannot be changed
+const age = 35;     // cannot be changed
 var oldWay = true;  // older way (avoid if possible)
