76afb9366dd0b6000585927f863dcda032528733
Jay Millena <jay.millena07@gmail.com>
2025-08-07 14:35:16 +0800

    Working on objects using JavaScript

---
+++ objects.js
@@ -0,0 +1,10 @@
+let user = {  
+    name: "Jay",                // Initializing an object with a name property
+    age: 35,                    // Adding an age property
+    greet: function() {         // Adding a method to greet     
+        console.log("Hello!");  // Outputs a greeting using the name property
+    }
+};
+
+console.log(user.name);         // Accessing the name property of the user object: Jay
+console.log(user.greet());      // Accessing the greet method of the user object: Hello!
