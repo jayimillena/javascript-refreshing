231d6c21f9a30ec9d44927fd2a505256bd60d34e
Jay Millena <jay.millena07@gmail.com>
2025-08-07 14:31:10 +0800

    Refreshing on Arrays

---
+++ arrays.js
@@ -0,0 +1,4 @@
+        let colors = ["red", "green", "blue", "yellow", "purple"];  // Initializing an array with color names
+        colors.push("orange");                                      // Adding a new color to the end of the array
+        colors.pop();                                               // Removing the last color from the array
+        console.log(colors[0]);                                     // Outputs: red
